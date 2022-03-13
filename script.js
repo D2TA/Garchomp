c8b48317cdb21e18b8cf4a05de6c655d95ad63d8
D2TA <66340798+D2TA@users.noreply.github.com>
2022-03-13 08:37:27 +0000

    - Name Styling

---
+++ script.js
@@ -0,0 +1,9 @@
+function onClickName1(){
+  document.getElementById('NameChangeOut').style.display = 'inline';
+  document.getElementById('NameChangeIn' ).style.display = 'none';
+}
+
+function onClickName2(){
+  document.getElementById('NameChangeOut').style.display = 'none';
+  document.getElementById('NameChangeIn' ).style.display = 'inline';  
+}
